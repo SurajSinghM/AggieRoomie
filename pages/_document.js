9d7ee8edf9586a796e759081bc5c4e80ba85b5d3
Suraj Singh <surajmsingh07@gmail.com>
2025-12-01 19:29:19 -0600

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,12 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+          {/* Google AdSense - insert sitewide in <Head> */}
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8324417197634076"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
